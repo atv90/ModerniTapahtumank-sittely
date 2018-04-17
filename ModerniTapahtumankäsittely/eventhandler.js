310541d6dc6fbf4eabcf7dc9ed10095a47e08345
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2018-04-17 08:52:45 +0300

    Lisättiin kaksi eri funktiota ja tapahtuman kuuntelu

--- ModerniTapahtumankäsittely/eventhandler.js
+++ ModerniTapahtumankäsittely/eventhandler.js
@@ -10,3 +10,18 @@
                 };
 				
 // tähän alas omat jutut
+
+//terve funktio, mikä tulostaa alertin
+function terve() {
+    alert("Tämä käynnistyi modernisti");
+}
+//tapahtuman kuuntelu
+window.onload = function () {
+    var oDiv = document.getElementById("xxx");
+    EventUtil.addEventHandler(oDiv, "click", terve);
+    EventUtil.addEventHandler(oDiv, "click", title);
+}
+//lisätään toinen funktio
+function title() {
+    alert(document.title);
+}
