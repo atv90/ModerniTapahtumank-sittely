d742d5b5657b4b693a713a57afb40636d943b62c
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2018-04-17 08:41:37 +0300

    Moderni tapahtumankäsittely

---
+++ ModerniTapahtumankäsittely/eventhandler.js
@@ -0,0 +1,12 @@
+﻿ var EventUtil = new Object;
+                EventUtil.addEventHandler = function (oTarget, sEventType, fnHandler) {
+                    if (oTarget.addEventListener) {
+                        oTarget.addEventListener(sEventType, fnHandler, false);
+                    } else if (oTarget.attachEvent) {
+                        oTarget.attachEvent("on" + sEventType, fnHandler);
+                    } else {
+                        oTarget["on" + sEventType] = fnHandler;
+                    }
+                };
+				
+// tähän alas omat jutut
